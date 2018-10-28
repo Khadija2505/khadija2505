b3c5b2a711694b613e381ac558eceb04bb8a6bed
khadija2505 <khadija.boumzough18@gmail.com>
2018-10-28 17:11:08 +0100

    +traving.py

---
+++ traving.py
@@ -0,0 +1,13 @@
+def truncate_long_words(s, def truncate_long_words(s, num):
+    """Splits long words in string"""
+    words = s.split()
+    for word in words:
+        if len(word) > num:
+            for i in xrange(0, len(word), num):
+                yield word[i:i + num]
+        else:
+            yield word
+
+
+for t in truncate_long_words(s):
+    print t
